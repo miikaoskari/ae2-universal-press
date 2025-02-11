57d29ca6b2a213ee89a197fcf826b1c1d779fcc3
miikaoskari <miika.karjalainen13@gmail.com>
2025-02-12 00:02:50 +0200

    Add singleton class

---
+++ src/main/java/com/miikaoskari/ae2_universal_press/AEUPItems.java
@@ -0,0 +1,12 @@
+package com.miikaoskari.ae2_universal_press;
+
+import com.miikaoskari.ae2_universal_press.items.ItemUniversalPress;
+import net.minecraft.world.item.Item;
+
+public class AEUPItems {
+    public static ItemUniversalPress UNIVERSAL_PRESS;
+
+    public static void init() {
+        // Initialize items here
+    }
+}
