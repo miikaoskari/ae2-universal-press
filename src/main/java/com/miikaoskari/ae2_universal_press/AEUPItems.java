d1cda7fadc6fa605a44567c6f67e859e528fb000
miikaoskari <miika.karjalainen13@gmail.com>
2025-02-15 23:53:10 +0200

    Some sorting cleanups

--- src/main/java/com/miikaoskari/ae2_universal_press/AEUPItems.java
+++ src/main/java/com/miikaoskari/ae2_universal_press/AEUPItems.java
@@ -19,6 +19,10 @@ public class AEUPItems {
 
     // Register the Universal Press item
     public static final DeferredItem<Item> UNIVERSAL_PRESS = ITEMS.register("universal_press", () -> new ItemUniversalPress(new Item.Properties()));
+    // Register the Universal Press creative mode tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> UNIVERSAL_PRESS_TAB = CREATIVE_MODE_TABS.register("universal_press_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.ae2_universal_press")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> AEUPItems.UNIVERSAL_PRESS.get().getDefaultInstance()).displayItems((parameters, output) -> {
+        output.accept(AEUPItems.UNIVERSAL_PRESS.get());
+    }).build());
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
@@ -27,9 +31,4 @@ public class AEUPItems {
     public static void registerTabs(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
     }
-
-    // Creates a creative tab with the id "ae2_universal_press:example_tab"
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> UNIVERSAL_PRESS_TAB = CREATIVE_MODE_TABS.register("universal_press_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.ae2_universal_press")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> AEUPItems.UNIVERSAL_PRESS.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(AEUPItems.UNIVERSAL_PRESS.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    }).build());
 }
--- src/main/java/com/miikaoskari/ae2_universal_press/Ae2UniversalPress.java
+++ src/main/java/com/miikaoskari/ae2_universal_press/Ae2UniversalPress.java
@@ -15,15 +15,10 @@ import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Ae2UniversalPress.MODID)
 public class Ae2UniversalPress {
     public static final String MODID = "ae2_universal_press";
     private static final Logger LOGGER = LogUtils.getLogger();
-
-    public static ResourceLocation makeId(String id) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, id);
-    }
 
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Ae2UniversalPress(IEventBus modEventBus, ModContainer modContainer) {
@@ -37,28 +32,27 @@ public class Ae2UniversalPress {
         AEUPItems.registerTabs(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (Ae2_universal_press) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    public static ResourceLocation makeId(String id) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, id);
+    }
+
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
         LOGGER.info("Common setup for AE2 Universal Press");
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("Server starting for AE2 Universal Press");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
